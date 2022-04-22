a67810d89f069f3630bef599788caba6667c4ccc
denisesato <denise.vecino@gmail.com>
2022-04-21 23:45:25 -0300

    Refactoring for removing duplicated code.

--- simulate_logs_wth_drifts.py
+++ simulate_logs_wth_drifts.py
@@ -6,6 +6,23 @@ from pm4py.objects.log.obj import EventLog
 from datetime import datetime
 
 
+def simulate_log(net1, im1, fm1, i, timestamp, case_id):
+    eventlog = simulator.apply(net1, im1, fm1, variant=simulator.Variants.BASIC_PLAYOUT,
+                                    parameters={
+                                        simulator.Variants.BASIC_PLAYOUT.value.Parameters.NO_TRACES: i,
+                                        simulator.Variants.BASIC_PLAYOUT.value.Parameters.INITIAL_TIMESTAMP:
+                                            timestamp,
+                                        simulator.Variants.BASIC_PLAYOUT.value.Parameters.INITIAL_CASE_ID: case_id})
+    # get the timestamp of the last event
+    last_event = eventlog[-1][-1]
+    last_datetime = last_event['time:timestamp']
+    last_timestamp = datetime.timestamp(last_datetime)
+    timestamp = last_timestamp + 1
+    # increment the case id
+    case_id = case_id + i
+    return eventlog, timestamp, case_id
+
+
 def generate_log_with_drifts(net1, im1, fm1, net2, im2, fm2, intervals, out_folder, out_filename):
     if not os.path.exists(out_folder):
         os.makedirs(out_folder)
@@ -16,35 +33,11 @@ def generate_log_with_drifts(net1, im1, fm1, net2, im2, fm2, intervals, out_fold
     case_id = 1
     for i in intervals:
         if base_model:
-            eventlog.extend(simulator.apply(net1, im1, fm1, variant=simulator.Variants.BASIC_PLAYOUT,
-                                            parameters={
-                                                simulator.Variants.BASIC_PLAYOUT.value.Parameters.NO_TRACES: i,
-                                                simulator.Variants.BASIC_PLAYOUT.value.Parameters.INITIAL_TIMESTAMP:
-                                                    timestamp,
-                                                simulator.Variants.BASIC_PLAYOUT.value.Parameters.INITIAL_CASE_ID: case_id}))
-            # get the timestamp of the last event
-            last_event = eventlog[-1][-1]
-            last_datetime = last_event['time:timestamp']
-            last_timestamp = datetime.timestamp(last_datetime)
-            timestamp = last_timestamp + 1
-            # increment the case id
-            case_id = case_id + i
+            new_log, timestamp, case_id = simulate_log(net1, im1, fm1, i, timestamp, case_id)
+            eventlog.extend(new_log)
         else:
-            eventlog.extend(simulator.apply(net2, im2, fm2, variant=simulator.Variants.BASIC_PLAYOUT,
-                                            parameters={
-                                                simulator.Variants.BASIC_PLAYOUT.value.Parameters.NO_TRACES: i,
-                                                simulator.Variants.BASIC_PLAYOUT.value.Parameters.INITIAL_TIMESTAMP:
-                                                    timestamp,
-                                                simulator.Variants.BASIC_PLAYOUT.value.Parameters.INITIAL_CASE_ID:
-                                                    case_id}
-                                            ))
-            # get the timestamp of the last event
-            last_event = eventlog[-1][-1]
-            last_datetime = last_event['time:timestamp']
-            last_timestamp = datetime.timestamp(last_datetime)
-            timestamp = last_timestamp + 1
-            # increment the case id
-            case_id = case_id + i
+            new_log, timestamp, case_id = simulate_log(net2, im2, fm2, i, timestamp, case_id)
+            eventlog.extend(new_log)
         base_model = not base_model
 
     eventlog = EventLog(eventlog)
@@ -53,8 +46,10 @@ def generate_log_with_drifts(net1, im1, fm1, net2, im2, fm2, intervals, out_fold
 
 if __name__ == '__main__':
     folder = os.path.join('data', 'output')
+    drift_folder = os.path.join('data', 'output', 'drift')
     net1, im1, fm1 = pnml_importer.apply(os.path.join(folder, 'model1.pnml'))
     net2, im2, fm2 = pnml_importer.apply(os.path.join(folder, 'model2.pnml'))
     intervals = [10, 10, 10, 10]
-    drift_folder = os.path.join('data', 'output', 'drift')
-    generate_log_with_drifts(net1, im1, fm1, net2, im2, fm2, intervals, drift_folder, 'drift_log1')
+    generate_log_with_drifts(net1, im1, fm1, net2, im2, fm2, intervals, drift_folder, 'drift_log_teste')
+    intervals = [100, 50, 200, 350]
+    generate_log_with_drifts(net1, im1, fm1, net2, im2, fm2, intervals, drift_folder, 'drift_log_teste2')
